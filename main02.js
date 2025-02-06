a4966a3177aad27acfc4f1ebe6e39ae742c27d46
carligar82 <carligar82@gmail.com>
2025-02-06 18:59:02 +0100

    Longitud contrasenya

---
+++ main02.js
@@ -0,0 +1,25 @@
+
+const mensaje = document.getElementsByTagName('h3')[0];
+let cont =""
+function onKeyDownHandler(event) {
+  var codigo = event.which || event.keyCode;
+  cont = cont + String.fromCharCode(codigo)
+  if (cont.length<8){
+    mensaje.className = "red"
+    mensaje.innerText = "Contraseña no segura"
+  }
+  if (cont.length>=8 && cont.length<=10){
+    mensaje.className = "yellow"
+    mensaje.innerText = "Contraseña insegura"
+  }
+  if (cont.length>10){
+    mensaje.className = "green"
+    mensaje.innerText = "Contraseña segura"
+  }
+  
+}
+
+
+   
+
+
