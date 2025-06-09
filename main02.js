a9c5f7ef3c8ad0065608f52a3d416a963f3fdc52
jose <jjoossee94@gmail.com>
2025-06-09 16:07:59 +0200

    Improve password strength check

--- main02.js
+++ main02.js
@@ -1,25 +1,19 @@
 
 const mensaje = document.getElementsByTagName('h3')[0];
-let cont =""
-function onKeyDownHandler(event) {
-  var codigo = event.which || event.keyCode;
-  if (codigo == 8)
-    cont = cont.slice(0, -1)
-  else
-    cont = cont + String.fromCharCode(codigo)
-  if (cont.length<8){
-    mensaje.className = "red"
-    mensaje.innerText = "Contraseña no segura"
+
+function updateStrength(event) {
+  const length = event.target.value.length;
+
+  if (length < 8) {
+    mensaje.className = "red";
+    mensaje.innerText = "Contraseña no segura";
+  } else if (length <= 10) {
+    mensaje.className = "yellow";
+    mensaje.innerText = "Contraseña insegura";
+  } else {
+    mensaje.className = "green";
+    mensaje.innerText = "Contraseña segura";
   }
-  if (cont.length>=8 && cont.length<=10){
-    mensaje.className = "yellow"
-    mensaje.innerText = "Contraseña insegura"
-  }
-  if (cont.length>10){
-    mensaje.className = "green"
-    mensaje.innerText = "Contraseña segura"
-  }
-  
 }
 
 
